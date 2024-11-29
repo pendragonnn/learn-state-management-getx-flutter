bf86629ff29b99efc24cf00f853bb1f40a621fc4
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-11-29 14:23:35 +0700

    add: stateless vs stateful vs state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,221 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+// state management getx implementation
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return GetMaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// stateless implementation
+// class HomePage extends StatelessWidget {
+//   // const HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+//   int data = 0;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue[300],
+//         title: Center(
+//           child: Text(
+//             "Stateless",
+//             style: TextStyle(
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               "${data}", // nilai data yang ditampilkan tidak berubah,namun aslinya jika dilihat di debug console nilainya berubah
+//               // diperlukan stateful component agar nilai tampilan data bisa berubah sesuai dengan tombol yang ditekan
+//               style: TextStyle(
+//                 fontSize: 50,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     data -= 1;
+//                     print(data);
+//                   },
+//                   child: Text("-"),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     data += 1;
+//                     print(data);
+//                   },
+//                   child: Text("+"),
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  final String title;
+// stateful implementation
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// class _HomePageState extends State<HomePage> {
+//   int data = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     // kekurangan stateful
+//     // selalu melakukan render ulang dalam 1 komponen (Scaffold)
+//     print("rendering");
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue[300],
+//         title: Center(
+//           child: Text(
+//             "Stateful",
+//             style: TextStyle(
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               "${data}",
+//               style: TextStyle(
+//                 fontSize: 50,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       data += 1;
+//                     });
+//                   },
+//                   child: Text(
+//                     "+",
+//                   ),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       data -= 1;
+//                     });
+//                   },
+//                   child: Text(
+//                     "-",
+//                   ),
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// state management getx implementation
+class HomePage extends StatelessWidget {
+  // const HomePage({super.key});
+  final myController = Get.put(Controller());
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // tidak di-build ulang
+    print("render ulang?");
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.blue[300],
+        title: Center(
+          child: Text(
+            "State Management GetX",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // bungkus dengan Obx agar terlihat perubahan
+            Obx(
+              () => Text(
+                "${myController.data.value}",
+                style: TextStyle(
+                  fontSize: 50,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(
+              height: 20,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    myController.decrement();
+                  },
+                  child: Text(
+                    "-",
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    myController.increment();
+                  },
+                  child: Text(
+                    "+",
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Controller extends GetxController {
+  var data = 0.obs; // .bs => state yang akan selalu dipantau perubahannya
+  increment() => data++;
+  decrement() => data--;
 }
